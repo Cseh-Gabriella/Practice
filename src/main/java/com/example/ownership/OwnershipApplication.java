3e16f0787c3a540247c14de733d90bee98b2de28
Cseh-Simon Gabriella <csehsimon.gabriella@gmail.com>
2023-11-13 19:50:55 +0100

    Start

---
+++ src/main/java/com/example/ownership/OwnershipApplication.java
@@ -0,0 +1,13 @@
+package com.example.ownership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnershipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ownership/OwnershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ownership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
